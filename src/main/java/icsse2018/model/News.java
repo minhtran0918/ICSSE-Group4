afb83bdfe038aadc1f312411414a97f74a364bd2
Lưu Phước Phương Nam <luunamten@gmail.com>
2018-03-29 18:05:00 +0700

    fff

--- src/main/java/icsse2018/model/News.java
+++ src/main/java/icsse2018/model/News.java
@@ -6,10 +6,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-@Entity(name="news")
+@Entity
+@Table(name="news")
 public class News {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
