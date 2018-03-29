546d1fb094bf516bda9e6136d63f880cd5ee0dec
Lưu Phước Phương Nam <luunamten@gmail.com>
2018-03-29 18:25:14 +0700

    comm

--- src/main/java/icsse2018/Application.java
+++ src/main/java/icsse2018/Application.java
@@ -12,20 +12,18 @@ import icsse2018.storage.StorageProperties;
 import icsse2018.storage.StorageService;
 
 @SpringBootApplication
-@EnableConfigurationProperties(StorageProperties.class)
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 
 	}
-	@Bean
+	/*@Bean
 	CommandLineRunner init(StorageService storageService) {
 		return (args) -> {
             storageService.deleteAll();
             storageService.init();
 		};
-	}
+	}*/
 
 }
